d44df840960fd35d0dd9cadcc0f51776fb62a289
SeungyoupBaek <bsy8220@naver.com>
2014-01-06 12:50:20 +0900

    Initial Commit

---
+++ Exe 3.2.3Tests/Exe_3_2_3Tests.m
@@ -0,0 +1,34 @@
+//
+//  Exe_3_2_3Tests.m
+//  Exe 3.2.3Tests
+//
+//  Created by SDT-1 on 2014. 1. 6..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Exe_3_2_3Tests : XCTestCase
+
+@end
+
+@implementation Exe_3_2_3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
